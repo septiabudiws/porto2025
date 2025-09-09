3a30d0e1ff06716c1cbc0995ea2287508395e216
septiabudiws <septiabudiws@gmail.com>
2025-09-09 13:18:49 +0700

    feat:foreach kategori blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategoriblog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -13,6 +14,9 @@ class BlogController extends Controller
 
     public function create()
     {
-        return view('admin.blog.blog-create');
+        $data = [
+            'kategori' => Kategoriblog::all()
+        ];
+        return view('admin.blog.blog-create', $data);
     }
 }
